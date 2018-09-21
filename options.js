618b731f190711b087eb5dba236d8ae53bc559e0
NathanMH <nathan@musicnate.ca>
2018-09-20 21:31:07 -0400

    In middle of transition from hover to onfocus/focusout

---
+++ options.js
@@ -0,0 +1,83 @@
+function saveOptions(e) {
+    e.preventDefault();
+    browser.storage.sync.set({
+        bookmarkBackgroundColor: document.querySelector("#bookmarkBackgroundColor").value,
+        bookmarkTextColor: document.querySelector("#bookmarkTextColor").value,
+        // bookmarkBorderColor: document.querySelector("#bookmarkBorderColor").value,
+
+        openTabsBackgroundColor: document.querySelector("#openTabsBackgroundColor").value,
+        openTabsTextColor: document.querySelector("#openTabsTextColor").value,
+        // openTabsBorderColor: document.querySelector("#openTabsBorderColor").value,
+
+        focusItemBackgroundColor: document.querySelector("#focusItemBackgroundColor").value,
+        focusItemBorderColor: document.querySelector("#focusItemBorderColor").value,
+        focusItemTextColor: document.querySelector("#focusItemTextColor").value,
+    });
+}
+
+function restoreOptions() {
+
+    function setCurrentChoice(result) {
+        document.querySelector("#bookmarkBackgroundColor").value = result.bookmarkBackgroundColor || "#CEE5F8";
+        document.querySelector("#bookmarkTextColor").value = result.bookmarkTextColor || "black";
+        // document.querySelector("#bookmarkBorderColor").value = result.bookmarkBorderColor || "#0060DF";
+
+        document.querySelector("#openTabsBackgroundColor").value = result.openTabsBackgroundColor || "white";
+        document.querySelector("#openTabsTextColor").value = result.openTabsTextColor || "black";
+        // document.querySelector("#openTabsBorderColor").value = result.openTabsBorderColor || "#0060DF";
+
+        document.querySelector("#focusItemBackgroundColor").value = result.focusItemBackgroundColor || "white";
+        document.querySelector("#focusItemTextColor").value = result.focusItemTextColor || "black";
+        document.querySelector("#focusItemBorderColor").value = result.focusItemBorderColor || "#0060DF";
+
+    }
+
+    var getting = browser.storage.sync.get([
+        "bookmarkBackgroundColor",
+        "bookmarkTextColor",
+        // "bookmarkBorderColor",
+
+        "openTabsBackgroundColor",
+        "openTabsTextColor",
+        // "openTabsBorderColor"
+
+        "focusItemBackgroundColor",
+        "focusItemTextColor",
+        "focusItemBorderColor"
+    ]);
+    getting.then(setCurrentChoice);
+
+}
+
+function resetOptions() {
+
+    document.querySelector("#bookmarkBackgroundColor").value = "#CEE5F8";
+    document.querySelector("#bookmarkTextColor").value = "black";
+    // document.querySelector("#bookmarkBorderColor").value = "#0060DF";
+
+    document.querySelector("#openTabsBackgroundColor").value = "white";
+    document.querySelector("#openTabsTextColor").value = "black";
+    // document.querySelector("#openTabsBorderColor").value = "#0060DF";
+
+    document.querySelector("#focusItemBackgroundColor").value = "white";
+    document.querySelector("#focusItemTextColor").value = "black";
+    document.querySelector("#focusItemBorderColor").value = "#0060DF";
+
+    browser.storage.sync.set({
+        bookmarkBackgroundColor: "#CEE5F8",
+        bookmarkTextColor: "black",
+        // bookmarkBorderColor: "#0060DF",
+
+        openTabsBackgroundColor: "white",
+        openTabsTextColor: "black",
+        // openTabsBorderColor: "#0060DF"
+
+        focusItemBackgroundColor: "white",
+        focusItemTextColor: "black",
+        focusItemBorderColor: "#0060DF"
+    });
+}
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.getElementById("save").addEventListener("click", saveOptions);
+document.getElementById("reset").addEventListener("click", resetOptions);
--- tabs.js
+++ tabs.js
@@ -8,6 +8,36 @@ window.browser = (function () {
         window.chrome;
 })();
 
+function setOptColor(result) {
+    bookmarkBackgroundColor = result.bookmarkBackgroundColor;
+    bookmarkTextColor = result.bookmarkTextColor;
+    // bookmarkBorderColor = result.bookmarkBorderColor;
+
+    openTabsBackgroundColor = result.openTabsBackgroundColor;
+    openTabsTextColor = result.openTabsTextColor;
+    // openTabsBorderColor = result.openTabsBorderColor;
+
+    focusItemBackgroundColor = result.focusItemBackgroundColor;
+    focusItemTextColor = result.focusItemTextColor;
+    focusItemBorderColor = result.focusItemBorderColor;
+}
+
+var getting = browser.storage.sync.get([
+    "bookmarkBackgroundColor",
+    "bookmarkTextColor",
+    // "bookmarkBorderColor",
+
+    "openTabsBackgroundColor",
+    "openTabsTextColor",
+    // "openTabsBorderColor",
+
+    "focusItemBackgroundColor",
+    "focusItemTextColor",
+    "focusItemBorderColor"
+]);
+
+getting.then(setOptColor);
+
 function searchResultTabs() {
     // console.log("search working");
     resultsTabsList.textContent = '';
@@ -17,36 +47,112 @@ function searchResultTabs() {
     let allTitles = tabsTitles.concat(bookmarksTitles);
 
     let searchQuery = document.getElementById("find-input").value;
-    let fuzzyResultsAll = fuzzysort.go(searchQuery, allTitles);
+    // TODO Add option to customize threshold for scores
+    let fuzzyResultsAll = fuzzysort.go(searchQuery, allTitles, { threshold: -200 });
+    // console.log(fuzzyResultsAll);
 
-    for (i in fuzzyResultsAll) {
+    tabLink = document.createElement('a');
+    if (tabsTitles.includes(fuzzyResultsAll[0].target)) {
+        tabLink.style.backgroundColor = openTabsBackgroundColor;
+        tabLink.style.color = openTabsTextColor;
+        tabLink.style.border = "4px solid " + focusItemBorderColor;
+        var currTab = tabsObjects.filter(obj => obj.title === fuzzyResultsAll[0].target);
+        tabLink.textContent = currTab[0].title.slice(0, 64);
+        tabLink.setAttribute('href', currTab[0].id);
+        tabLink.setAttribute('window', currTab[0].winId);
+        tabLink.setAttribute('score', fuzzyResultsAll[0].score);
+        tabLink.classList.add('open-tab-link');
+        tabLink.onmouseenter = function () {
+            this.style.backgroundColor = focusItemBackgroundColor;
+            this.style.color = focusItemTextColor;
+            this.style.border = "4px solid " + focusItemBorderColor;
+        };
+        tabLink.onmouseleave = function () {
+            this.style.backgroundColor = openTabsBackgroundColor;
+            this.style.color = openTabsTextColor;
+            this.style.border = "";
+        };
+    } else if (bookmarksTitles.includes(fuzzyResultsAll[0].target)) {
+        tabLink.style.backgroundColor = bookmarkBackgroundColor;
+        tabLink.style.color = bookmarkTextColor;
+        tabLink.style.border = "4px solid " + focusItemBorderColor;
+        var currTab = bookmarksObjects.filter(obj => obj.title === fuzzyResultsAll[0].target);
+        tabLink.textContent = currTab[0].title.slice(0, 64);
+        tabLink.setAttribute('href', currTab[0].url);
+        tabLink.setAttribute('window', currTab[0].winId);
+        tabLink.classList.add('bookmark-link');
+        tabLink.onfocus = function () {
+            this.style.backgroundColor = focusItemBackgroundColor;
+            this.style.color = focusItemTextColor;
+            this.style.border = "4px solid " + focusItemBorderColor;
+        };
+        tabLink.onmouseleave = function () {
+            this.style.backgroundColor = bookmarkBackgroundColor;
+            this.style.color = bookmarkTextColor;
+            this.style.border = "";
+        };
+
+    }
+
+    for (i in fuzzyResultsAll.slice(1)) {
         // console.log(i);
         let tabLink = document.createElement('a');
 
         if (tabsTitles.includes(fuzzyResultsAll[i].target)) {
+            // Options
+            tabLink.style.backgroundColor = openTabsBackgroundColor;
+            tabLink.style.color = openTabsTextColor;
+
             // retrieve tab object from tabsObjects with matching title
             var currTab = tabsObjects.filter(obj => obj.title === fuzzyResultsAll[i].target);
-            // tabLink.textContent = currTab[0].title || currTab[0].id;
             tabLink.textContent = currTab[0].title.slice(0, 64);
             tabLink.setAttribute('href', currTab[0].id);
             tabLink.setAttribute('window', currTab[0].winId);
+            tabLink.setAttribute('score', fuzzyResultsAll[i].score);
             tabLink.classList.add('open-tab-link');
-            // console.log(tabLink);
+
+            tabLink.onfocus = function () {
+                this.style.backgroundColor = focusItemBackgroundColor;
+                this.style.color = focusItemTextColor;
+                this.style.border = "4px solid " + focusItemBorderColor;
+            };
+            tabLink.onfocusout = function () {
+                this.style.backgroundColor = openTabsBackgroundColor;
+                this.style.color = openTabsTextColor;
+                this.style.border = "";
+            };
+            // console.log(tabLink.getAttribute('score'));
         } else if (bookmarksTitles.includes(fuzzyResultsAll[i].target)) {
+
+            // Bookmark Options
+            tabLink.style.backgroundColor = bookmarkBackgroundColor;
+            tabLink.style.color = bookmarkTextColor;
+
             // retrieve bookmark object from bookmarksObjects with matching title
             var currTab = bookmarksObjects.filter(obj => obj.title === fuzzyResultsAll[i].target);
-            // tabLink.textContent = currTab[0].title || currTab[0].id;
             tabLink.textContent = currTab[0].title.slice(0, 64);
             tabLink.setAttribute('href', currTab[0].url);
             tabLink.setAttribute('window', currTab[0].winId);
             tabLink.classList.add('bookmark-link');
+
+            tabLink.onmouseenter = function () {
+                this.style.backgroundColor = focusItemBackgroundColor;
+                this.style.color = focusItemTextColor;
+                this.style.border = "4px solid " + focusItemBorderColor;
+            };
+            tabLink.onmouseleave = function () {
+                this.style.backgroundColor = bookmarkBackgroundColor;
+                this.style.color = bookmarkTextColor;
+                this.style.border = "";
+            };
+
             // console.log(tabLink);
         } else {
             // console.log("no known tab");
         }
 
         resultsTabsList.appendChild(tabLink);
-        colourFirst();
+        // colourFirst();
     }
 
     // Switch to first item with Enter (ie. don't put <CR> into text box)
